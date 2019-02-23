2a496ff1014810139afbf6fa1e73eff53af6eef6
Nochris <nochris@macbook-pro.mynet>
2019-02-23 21:04:18 +0000

    coded click_more and click next functions

--- TripAdvisorReviewParsing.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-import os
-
-url = requests.get("https://www.tripadvisor.co.uk/Restaurant_Review-g186338-d5861005-Reviews-or20-Rice_Republic-London_England.html")
-page_code = BeautifulSoup(url.content,"html.parser")
---
+++ code/ta_page_parsing.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+
+url = requests.get(
+    "https://www.tripadvisor.co.uk/Restaurant_Review-g186338-d5861005-Reviews-or20-Rice_Republic-London_England.html")
+page_code = BeautifulSoup(url.content, "html.parser")
