97e979735c6fb6ca47c8c0ce494dd961024b96b3
Nochris <nochris@macbook-pro.mynet>
2019-02-23 21:04:18 +0000

    coded click_more and click next functions

--- code/ta_chinese_restaurant_url_scraper.py
+++ code/ta_chinese_restaurant_url_scraper.py
@@ -1,18 +1,23 @@
-from TripAdvisorReviews import get_all_restaurant_href
-from TripAdvisorReviews import get_all_regions_href
+from ta_reviews import get_all_restaurant_href
+from ta_reviews import get_all_regions_href
 import os
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
+main_dir = os.path.abspath(os.path.join(dname, os.pardir))
+code_dir = main_dir + '/data'
 
 #get_all_regions_href()
 
-RegionUrls = open("TripsAdvisorRegionUrl.txt", "r")
+RegionUrls = open(code_dir & '"TripsAdvisorRegionUrl.txt', 'r')
 for region in RegionUrls:
     get_all_restaurant_href(region)
 
-Pring("Parsing All Completed")
+Pring('Parsing All Completed')
+
+
+
 #     print(region)
 #
 # pattern = re.compile(r"\w*\_\w*")
--- code/ta_click_more.py
+++ code/ta_click_more.py
@@ -5,6 +5,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
+def click_more(driver):
+    elem_more = driver.find_element_by_xpath('//span[@class="taLnk ulBlueLinks"]').click()
+    # elem_more[0].click()
+
+def click_next(driver):
+    elem_next = driver.find_element_by_xpath('//a[@class="nav next taLnk ui_button primary"]').click()
+
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
@@ -24,37 +31,45 @@ driver = webdriver.Chrome(chrome_options=chrome_options)
 # driver.set_window_size(1920,1080)
 # try:
 driver.get('https://www.tripadvisor.co.uk/Restaurant_Review-g186338-d5861005-Reviews-Rice_Republic-London_England.html')
-# elem = driver.find_elements_by_class_name('taLnk ulBlueLinks')
 # driver.implicitly_wait(5)
 # elem = driver.find_element_by_xpath("//span[@class='taLnk ulBlueLinks']").click()
 
+click_more(driver)
+click_next(driver)
 
-elem = driver.find_element_by_xpath("//span[@class='taLnk ulBlueLinks']")
+
+elem_next = driver.find_elements_by_xpath("//span[text()='Next']")
+elem_next
 # driver.find_element_by_xpath("//*[contains(text(), 'More')]").click()
 # driver.switch_to_frame(elem)
 # print(elem)
 # elem.move_to_element()
 # elem.send_keys(Keys.RETURN)
-action = webdriver.common.action_chains.ActionChains(driver)
+
+
+
+elem_next.get_attribute('innerHTML')
+
+# action = webdriver.common.action_chains.ActionChains(driver)
 #
 # for i in range(-15, 0):
 
-print(elem.location['x'], elem.location['y'])
-print(elem.size['width'],elem.size['height'])
-print(elem.text)
-# action.move_to_element_with_offset(elem, 0 , -elem.size['height']/2 + 1).click().perform()
-try:
-    for j in range(-100, 0):
-        print(0, j)
-        action.move_to_element_with_offset(elem, 0, j).click().perform()
-
-except Exception:
-        element = driver.find_elements_by_xpath("//p[@class='partial_entry']")
-        i = 1
-        for e in element:
-                print (str(i) + '\n')
-                i = i+1
-                print(e.text.encode("utf8"))
+# print(elem.location['x'], elem.location['y'])
+# print(elem.size['width'],elem.size['height'])
+# print(elem.text)
+# # action.move_to_element_with_offset(elem, 0 , -elem.size['height']/2 + 1).click().perform()
+# try:
+#     for j in range(-100, 0):
+#         print(0, j)
+#         action.move_to_element_with_offset(elem, 0, j).click().perform()
+#
+# except Exception:
+#         element = driver.find_elements_by_xpath("//p[@class='partial_entry']")
+#         i = 1
+#         for e in element:
+#                 print (str(i) + '\n')
+#                 i = i+1
+#                 print(e.text.encode("utf8"))
 
 
 # elem.click()
